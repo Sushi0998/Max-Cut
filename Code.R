276327fdf7397bd3f6c582ec361343727b50c60a
Suraj Shinde <99321988+Sushi0998@users.noreply.github.com>
2023-06-22 08:07:28 -0500

    Update Code.R

--- Code.R
+++ Code.R
@@ -1,7 +1,8 @@
-#reading data file" name to be changed"
+#reading data file.
 dat <- read.table(file ="C:/Users/SURAJ SHINDE/OneDrive/Desktop/TAMU Courses/probablity/graph1.txt", header = TRUE)
 #important values from data
 set.seed(123)
+
 Value<-max(dat)
 RowS <-nrow(dat)
 
@@ -32,12 +33,12 @@ for (i in 1:RowS){
 All_Max_value <-vector()
 #updating the best cut
 Best_cut <- list()
-#k = 10e6repetition, Beta (0,-1,-10)
+#k = 10e5 iterations, Beta (-1,0,1)
 
 count = 0 
 k <- 100000
-B<- 0                                          #value can be change to -1,0,1
-Best_value <- -1000                              #base value for the first iteration (reference in fro loop)
+B<- 0                                            #value can be change to -1,0,1
+Best_value <- -1000                              #base value for the first iteration (reference in for loop)
 
 for (i in 1:k){
   weight_proposed<-0                                        #INITIALISING weight_proposed
@@ -46,19 +47,19 @@ for (i in 1:k){
   
   select <- round(runif(1, min = 1, max = RowS))            #selecting 1 row in random 
   
-  flip <- current_conf[select] * (-1)                        #flipping the bit
+  flip <- current_conf[select] * (-1)                        #flipping the bit i.e. changing the -1 to 1 or 1 to -1, which creates a new configuration 
   
-  Proposed_conf = replace (current_conf,select,flip)         #creating a list for proposed configuration using the flipped bit,which will help us to get the weighted cut
+  Proposed_conf = replace (current_conf,select,flip)         #creating a list for proposed configuration using the flipped bit, which will help us to get the new weighted cut
   
   for (l in 1:RowS){
     
-    weight_current = 0.5*(dat$Weight[l])*(1-(current_conf[dat$i[l]]*current_conf[dat$j[l]]))+weight_current #weight of current cut
+    weight_current = 0.5*(dat$Weight[l])*(1-(current_conf[dat$i[l]]*current_conf[dat$j[l]]))+weight_current # weight of current cut
     
-    weight_proposed =0.5*(dat$Weight[l])*(1-(Proposed_conf[dat$i[l]]*Proposed_conf[dat$j[l]]))+weight_proposed #weight of proposed cut
+    weight_proposed =0.5*(dat$Weight[l])*(1-(Proposed_conf[dat$i[l]]*Proposed_conf[dat$j[l]]))+weight_proposed # weight of proposed cut
 
   }
 
-  #acceptance Function, using gibbs distribution
+  #acceptance Function, using gibbs distribution.
   accept_value <- (exp(-1*B*(weight_proposed - weight_current)))
   #if the value is greater than 1 we accept the proposed value and note all the max values
   if (accept_value >= 1){
@@ -83,8 +84,7 @@ for (i in 1:k){
   }  
 }
 
-#plotting max cut values of for k iterations 
-plot(All_Max_value)
-Best_value
-Best_conf
+plot(All_Max_value) # plotting all values for k = 10e5 iterations. 
+Best_value # displaying the max/highest value within all the iterations.
+Best_conf # displaying the configuration of the max/highest value within all iterations. 
 
